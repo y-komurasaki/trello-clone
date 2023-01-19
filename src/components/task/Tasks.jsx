9c2b7c6b632feb85d9947e42f7596b2c44572022
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-01-19 16:59:46 +0900

    ✨ FirstCommit

---
+++ src/components/task/Tasks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Task } from './Task';
+
+export const Tasks = ({ taskList }) => {
+  return (
+  <div>
+     {taskList.map((task) => (
+        <div>
+          <Task task={task} />
+        </div>
+     ))}
+    </div>
+  );
+};

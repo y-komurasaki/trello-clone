25a1a0d20d4cc0b6b2a8add4435db7813f2b520e
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-01-20 11:49:53 +0900

    ✨ タスクのドラック&ドロップ機能実装

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { Task } from './Task';
 
+const reorder = (taskList, startIndex, endIndex) => {
+  //タスクを並び変える
+  const remove = taskList.splice(startIndex, 1);
+  taskList.splice(endIndex, 0, remove[0]);
+};
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDragEnd = (result) => {
+
+
+    reorder(taskList, result.source.index, result.destination.index);
+
+    setTaskList(taskList);
+  };
   return (
   <div>
-     {taskList.map((task) => (
-        <div>
-          <Task task={task} taskList={taskList} setTaskList={setTaskList}/>
-        </div>
-     ))}
+    <DragDropContext onDragEnd={handleDragEnd}>
+       <Droppable droppableId="droppable">
+         {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {taskList.map((task, index) => (
+                <div key={task.id}>
+                  <Task 
+                    index={index} 
+                    task={task} 
+                    taskList={taskList} 
+                    setTaskList={setTaskList}
+                  />
+                </div>
+            ))}
+            {provided.placeholder}
+          </div>
+         )}
+
+       </Droppable>
+    </DragDropContext>
     </div>
   );
 };
